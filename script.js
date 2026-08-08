672d9f1269ec967accc6de48b7636573575af676
Mayank Kumar <103820518+thismayank1@users.noreply.github.com>
2026-08-08 16:49:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,22 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+
+	const prices=document.querySelectorAll(".prices");
+	let total=0;
+
+	price.forEach((price)=>{
+		total +=Number(price.textContent);
+
+		const newRow=document.createElement("tr");
+		const totalBox=document.createElement("td");
+		totalBox.textContent=`Total Price:${total}`;
+		newRow.appendChild(totalBox);
+
+		document.querySelector("table").appendChild(newRow);
+	})
+
+	
   
 };
 
